57a4b18707d3709d15344479877470464ebf340d
Fabian <lucasfabian34319611@gmail.com>
2024-11-21 23:01:16 -0300

    Projeto com todos os pacotes, as telas de cadastro e o Util.

---
+++ src/tools/Util.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tools;
+
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JTextField;
+
+public class Util {
+
+    public static void habilitar(boolean valor, JComponent... comp) {
+        for (int i = 0; i < comp.length; i++) {
+            comp[i].setEnabled(valor);
+        }
+
+    }
+
+    public static void limpar(JComponent... comp) {
+        for (int i = 0; i < comp.length; i++) {
+            if (comp[i] instanceof JTextField) {
+                ((JTextField) comp[i]).setText("");
+            }
+            if (comp[i] instanceof JComboBox) {
+                ((JComboBox) comp[i]).setSelectedIndex(-1);
+            }
+        }
+    }
+}
+
+//limpar textfild setext("");
+//limpar combobox setselectedindex(-1);
+//quem o jcomponent foi
+//usar instanceof
+
+//criar jdlgpedidos
